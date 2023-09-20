f54c29111cf7afee5608676cf414c2e657504f45
RFozzy688 <pokerfozzy@gmail.com>
2023-09-21 01:30:51 +0300

    просмотр изображений и пролистывание вправо/влево

--- hw_16_09_23/MainWindow.xaml.cs
+++ hw_16_09_23/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,78 @@ namespace hw_16_09_23
     /// </summary>
     public partial class MainWindow : Window
     {
+        int _index;
+        string[] _path;
         public MainWindow()
         {
             InitializeComponent();
+
+            _index = 0;
+            _path = Directory.GetFiles(@"G:\ШАГ\WPF\_hw\hw_16_09_23\hw_16_09_23\img");
+
+            FullImage.Source = GetPath();
+        }
+
+        private void GridLeft_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (_index - 1 >= 0)
+            {
+                GridLeft.Opacity = 1;
+            }
+        }
+        private void GridLeft_MouseLeave(object sender, MouseEventArgs e)
+        {
+            GridLeft.Opacity = 0;
+        }
+        private void GridRight_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (_index + 1 != _path.Length)
+            {
+                GridRight.Opacity = 1;
+            }
+        }
+        private void GridRight_MouseLeave(object sender, MouseEventArgs e)
+        {
+            GridRight.Opacity = 0;
+        }
+        BitmapImage GetPath()
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(_path[_index]);
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
+        private void GridLeft_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_index - 1 >= 0)
+            {
+                _index--;
+                FullImage.Source = GetPath();
+            }
+        }
+        private void GridRight_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_index + 1 <= _path.Length - 1)
+            {
+                _index++;
+                FullImage.Source = GetPath();
+            }
+        }
+        private void GridLeft_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_index - 1 < 0)
+            {
+                GridLeft.Opacity = 0;
+            }
+        }
+
+        private void GridRight_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_index + 1 == _path.Length)
+            {
+                GridRight.Opacity = 0;
+            }
         }
     }
 }
